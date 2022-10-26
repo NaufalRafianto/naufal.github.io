1dd475d6271a5b2fc1803ab961650d50b7693ece
NaufalRafianto <naufl.rafianto21@gmail.com>
2022-10-26 20:36:26 +0700

    v.1.1.0 Tambah Slider

---
+++ App.js
@@ -0,0 +1,76 @@
+$(document).ready(() => {
+    const home = $("#home_button")
+    const about = $("#about_button")
+    const work = $("#work_button")
+    const contact = $("#contact_button")
+    const activeCircle = $("#active_circle")
+
+    const setCSS = (id, property, value) => {
+        return id.css(property, value);
+    }
+
+    if (screen.width <= 800) {
+        setCSS(activeCircle, "left", "0")
+        setCSS(activeCircle, "bottom", "5px")
+    } else {
+        setCSS(activeCircle, "top", "0")
+        setCSS(activeCircle, "left", "5px")
+    }
+
+    const slider = (button, link) => {
+
+        button.click(() => {
+            window.location.href = link
+            if (screen.width <= 800) {
+                setCSS(activeCircle, "left", "0")
+                setCSS(activeCircle, "bottom", "7.5px")
+                if (button === home) {
+                    setCSS(activeCircle, "left", "0")
+                    setCSS(activeCircle, "bottom", "7.5px")
+                }
+                else if (button === about) {
+                    setCSS(activeCircle, "left", "100px")
+                    setCSS(activeCircle, "bottom", "7.5px")
+
+                }
+                else if (button === work) {
+                    setCSS(activeCircle, "left", "210px")
+                    setCSS(activeCircle, "bottom", "7.5px")
+
+                }
+                else if (button === contact) {
+                    setCSS(activeCircle, "left", "320px")
+                    setCSS(activeCircle, "bottom", "7.5px")
+
+                }
+            }
+            else {
+                setCSS(activeCircle, "top", "0")
+                setCSS(activeCircle, "left", "5px")
+
+                if (button === home) {
+                    setCSS(activeCircle, "top", "0")
+                }
+                else if (button === about) {
+                    setCSS(activeCircle, "top", "150px")
+
+                }
+                else if (button === work) {
+                    setCSS(activeCircle, "top", "310px")
+
+                }
+                else if (button === contact) {
+                    setCSS(activeCircle, "top", "460px")
+
+                }
+            }
+        })
+    }
+
+    slider(home, "#header")
+    slider(about, "#about")
+    slider(work, "#work")
+    slider(contact, "#contact")
+
+
+})
